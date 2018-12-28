31537485214bcd91968a17b9cde7c91341cd4fa8
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-28 13:20:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 def line(katz_deli)
   katz_deli = []
-  while katz_deli.length == 0
+  if katz_deli.length == 0
     puts "The line is currently empty."
-  end
+  else
 end
     
